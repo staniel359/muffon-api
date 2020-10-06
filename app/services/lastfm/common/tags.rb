dadb8f0cfb3c24cf15066cb6e9b281113c2af236
staniel359 <staniel359@gmail.com>
2020-10-06 19:06:11 +0300

    Add Artists/Albums/Tracks Tags

---
+++ app/services/lastfm/common/tags.rb
@@ -0,0 +1,41 @@
+module LastFM
+  module Common
+    class Tags < LastFM::Base
+      def call
+        return not_found_data if parsed_response.blank?
+
+        { tags: tags_data }
+      end
+
+      private
+
+      def parsed_response
+        @parsed_response ||= JSON.parse(
+          api_response("#{@args.model}.getTopTags")
+        )['toptags']
+      end
+
+      def tags_data
+        {
+          artist: parsed_response['@attr']['artist'],
+          album: add_data_if_model('album'),
+          track: add_data_if_model('track'),
+          tags: tags
+        }.compact
+      end
+
+      def add_data_if_model(model)
+        parsed_response['@attr'][model] if @args.model == model
+      end
+
+      def tags
+        parsed_response['tag'].map do |t|
+          {
+            name: t['name'],
+            count: t['count']
+          }
+        end
+      end
+    end
+  end
+end
---
+++ spec/features/last_fm/common/tags_spec.rb
@@ -0,0 +1,140 @@
+require 'rails_helper'
+
+RSpec.describe LastFM::Common::Tags do
+  subject { described_class }
+
+  describe 'successful processing' do
+    context 'when artist exists' do
+      let(:output) do
+        VCR.use_cassette 'lastfm/common/tags/artist/success' do
+          subject.call(
+            artist: 'wild nothing', model: 'artist'
+          )
+        end
+      end
+
+      it { expect(output).to eq(Helpers::LastFM.tags_artist_data) }
+    end
+
+    context 'when album exists' do
+      let(:output) do
+        VCR.use_cassette 'lastfm/common/tags/album/success' do
+          subject.call(
+            artist: 'wild nothing',
+            album: 'nocturne',
+            model: 'album'
+          )
+        end
+      end
+
+      it { expect(output).to eq(Helpers::LastFM.tags_album_data) }
+    end
+
+    context 'when track exists' do
+      let(:output) do
+        VCR.use_cassette 'lastfm/common/tags/track/success' do
+          subject.call(
+            artist: 'wild nothing',
+            track: 'chinatown',
+            model: 'track'
+          )
+        end
+      end
+
+      it { expect(output).to eq(Helpers::LastFM.tags_track_data) }
+    end
+  end
+
+  describe 'no processing' do
+    let(:not_found_data) { Helpers::LastFM.not_found_data }
+
+    context 'when no model name given' do
+      let(:output) do
+        VCR.use_cassette 'lastfm/common/tags/no_model' do
+          subject.call(
+            artist: 'wild nothing', model: nil
+          )
+        end
+      end
+
+      it { expect(output).to eq(not_found_data) }
+    end
+
+    context 'when no artist name given' do
+      let(:output) do
+        VCR.use_cassette 'lastfm/common/tags/artist/no_artist_name' do
+          subject.call(
+            artist: nil, model: 'artist'
+          )
+        end
+      end
+
+      it { expect(output).to eq(not_found_data) }
+    end
+
+    context 'when wrong artist name' do
+      let(:output) do
+        VCR.use_cassette 'lastfm/common/tags/artist/wrong_artist_name' do
+          subject.call(
+            artist: Helpers::LastFM::RANDOM_STRING,
+            model: 'artist'
+          )
+        end
+      end
+
+      it { expect(output).to eq(not_found_data) }
+    end
+
+    context 'when no album title given' do
+      let(:output) do
+        VCR.use_cassette 'lastfm/common/tags/album/no_album_title' do
+          subject.call(
+            artist: 'wild nothing', album: nil, model: 'album'
+          )
+        end
+      end
+
+      it { expect(output).to eq(not_found_data) }
+    end
+
+    context 'when wrong album title' do
+      let(:output) do
+        VCR.use_cassette 'lastfm/common/tags/album/wrong_album_title' do
+          subject.call(
+            artist: 'wild nothing',
+            album: Helpers::LastFM::RANDOM_STRING,
+            model: 'album'
+          )
+        end
+      end
+
+      it { expect(output).to eq(not_found_data) }
+    end
+
+    context 'when no track title given' do
+      let(:output) do
+        VCR.use_cassette 'lastfm/common/tags/track/no_track_title' do
+          subject.call(
+            artist: 'wild nothing', track: nil, model: 'track'
+          )
+        end
+      end
+
+      it { expect(output).to eq(not_found_data) }
+    end
+
+    context 'when wrong track title' do
+      let(:output) do
+        VCR.use_cassette 'lastfm/common/tags/track/wrong_track_title' do
+          subject.call(
+            artist: 'wild nothing',
+            track: Helpers::LastFM::RANDOM_STRING,
+            model: 'track'
+          )
+        end
+      end
+
+      it { expect(output).to eq(not_found_data) }
+    end
+  end
+end
