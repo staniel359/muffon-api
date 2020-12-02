0ae541a2744d417ea2b37a1990674bf9de25710e
staniel359 <staniel359@gmail.com>
2020-12-02 18:09:34 +0300

    Add ID to Tag tracks

--- app/services/lastfm/tag/tracks.rb
+++ app/services/lastfm/tag/tracks.rb
@@ -6,14 +6,33 @@ module LastFM
       def tracks
         tracks_list.map do |t|
           {
-            title: t.css('.chartlist-name a').text,
-            artist: t.css('.chartlist-artist a').text
+            id: track_id(t),
+            title: title(t),
+            artist: artist(t)
           }
         end
       end
 
       def tracks_list
         response_data.css('.chartlist-row')
+      end
+
+      def track_id(track)
+        ::Track.with_artist_id_title(
+          artist_id(track), title(track)
+        ).id
+      end
+
+      def artist_id(track)
+        ::Artist.with_name(artist(track)).id
+      end
+
+      def artist(track)
+        track.css('.chartlist-artist a').text
+      end
+
+      def title(track)
+        track.css('.chartlist-name a').text
       end
     end
   end
