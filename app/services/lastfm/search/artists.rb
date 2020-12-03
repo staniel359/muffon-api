06c23bbc4be52915a634b14d6feda3b9974ac28a
staniel359 <staniel359@gmail.com>
2020-12-03 22:13:44 +0300

    Change Search Artists logic

--- app/services/lastfm/search/artists.rb
+++ app/services/lastfm/search/artists.rb
@@ -1,64 +1,23 @@
 module LastFM
   module Search
-    class Artists < LastFM::Web
+    class Artists < LastFM::Search::Base
       private
 
-      def primary_args
-        [@args.query]
+      def model_params
+        { artist: @args.query }
       end
 
-      def no_data?
-        response_data.css('.artist-result').blank?
+      def model_name
+        'artist'
       end
 
-      def link
-        'https://www.last.fm/search/artists'
-      end
-
-      def params
-        { q: @args.query }
-      end
-
-      def data
-        { search: search_data }
-      end
-
-      def search_data
-        { artists: artists_data }
-      end
-
-      def artists_data
-        artists_list.map do |a|
+      def results_data
+        results.last(limit).map do |r|
           {
-            name: name(a),
-            image: image(a),
-            listeners_count: listeners_count(a)
+            name: r['name'],
+            listeners_count: r['listeners'].to_i
           }
         end
-      end
-
-      def artists_list
-        response_data.css('.artist-result').first(limit)
-      end
-
-      def limit
-        (@args.limit || 20).to_i
-      end
-
-      def name(artist)
-        artist.css('.artist-result-heading a')[0]['title']
-      end
-
-      def image(artist)
-        artist.css(
-          '.artist-result-image img'
-        )[0]['src'].sub('avatar70s', '64s')
-      end
-
-      def listeners_count(artist)
-        artist.css(
-          '.artist-result-listeners'
-        ).text.scan(/\d+/).join.to_i
       end
     end
   end
