324f0bf595c32b1762954454bcb2a53f42c426cb
staniel359 <staniel359@gmail.com>
2021-02-02 15:36:10 +0300

    Refactor VCR helper

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -5,23 +5,26 @@ VCR.configure do |c|
   c.hook_into :webmock
 
   keys = [
-    %w[lastfm api_key],
-    %w[youtube api_key],
-    %w[vk page_id],
-    %w[vk email],
-    %w[vk password],
-    %w[discogs api_key],
-    %w[discogs api_secret],
-    %w[spotify api_key],
-    %w[soundcloud api_key],
-    %w[deezer test_track_token],
-    %w[deezer test_api_token],
-    %w[deezer test_license_token]
+    %i[lastfm api_key],
+    %i[youtube api_key],
+    %i[vk page_id],
+    %i[vk email],
+    %i[vk password],
+    %i[discogs api_key],
+    %i[discogs api_secret],
+    %i[spotify api_key],
+    %i[soundcloud api_key],
+    %i[deezer test_track_token],
+    %i[deezer test_api_token],
+    %i[deezer test_license_token]
   ]
 
-  keys.each do |a|
-    filter_name = "<#{a[0]}_#{a[1]}>".upcase
-    value = Rails.application.credentials.send(a[0])[a[1].to_sym]
-    c.filter_sensitive_data(filter_name) { CGI.escape(value.to_s) }
+  keys.each do |arr|
+    filter_name = "<#{arr[0]}_#{arr[1]}>".upcase
+    value = Rails.application.credentials.dig(arr[0], arr[1])
+
+    c.filter_sensitive_data(filter_name) do
+      CGI.escape(value.to_s)
+    end
   end
 end
