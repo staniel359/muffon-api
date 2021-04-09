dba69a8b39d9aef9d7996e0b79295ac104c3d7bb
staniel359 <staniel359@gmail.com>
2021-04-09 22:16:10 +0300

    Change Bandcamp::Search::Tracks response format

--- app/services/bandcamp/search/tracks.rb
+++ app/services/bandcamp/search/tracks.rb
@@ -16,8 +16,13 @@ module Bandcamp
           title: title(item),
           artist: artist_data(item),
           images: images_data(item[:image]),
-          link: link_data(item[:link]).slice(:title, :artist)
+          link: link_data(item[:link]).slice(:title, :artist),
+          audio: audio_data
         }
+      end
+
+      def audio_data
+        { source: 'bandcamp' }
       end
     end
   end
