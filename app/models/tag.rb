63ab016a56a43b2492fd0178faf26eea10e19a4a
staniel359 <staniel359@gmail.com>
2021-11-13 11:22:45 +0300

    Add Tag model

---
+++ app/models/tag.rb
@@ -0,0 +1,16 @@
+class Tag < ApplicationRecord
+  validates :name,
+            presence: true,
+            uniqueness: true
+
+  class << self
+    def with_name(name)
+      where(
+        'LOWER(name) = ?',
+        name.strip.downcase
+      ).first_or_create(
+        name: name
+      )
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_01_060105) do
+ActiveRecord::Schema.define(version: 2021_11_13_080250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -216,6 +216,12 @@ ActiveRecord::Schema.define(version: 2021_11_01_060105) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "deleted", default: false
     t.index ["profile_id"], name: "index_recommendations_on_profile_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "tracks", force: :cascade do |t|
