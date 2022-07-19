e4470b4b9a7ca942472122a336e36901ffcf647b
staniel359 <staniel359@gmail.com>
2022-07-19 19:09:15 +0300

    Fix artist image_url migrations bug

--- db/migrate/20211230205226_remove_image_url_column_from_models.rb
+++ db/migrate/20211230205226_remove_image_url_column_from_models.rb
@@ -1,6 +1,5 @@
 class RemoveImageUrlColumnFromModels < ActiveRecord::Migration[7.0]
   def change
-    remove_column :artists, :image_url
     remove_column :bookmark_albums, :image_url
     remove_column :bookmark_tracks, :image_url
     remove_column :favorite_albums, :image_url
