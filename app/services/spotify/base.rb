344bc625709868394f3bcee6e6c5e6f4ab402a27
staniel359 <staniel359@gmail.com>
2022-11-06 22:21:13 +0300

    Change Spotify track audio logic

--- app/services/spotify/base.rb
+++ app/services/spotify/base.rb
@@ -2,7 +2,8 @@ module Spotify
   class Base < Muffon::Base
     BASE_LINK = 'https://api.spotify.com/v1'.freeze
     SOURCE_NAME = 'spotify'.freeze
-    COUNTRY_CODE = 'GB'.freeze
+    COUNTRY_CODE = 'US'.freeze
+
     include Muffon::Utils::Global
 
     def call
