110992f41b32a579b463aa41ac7c42e5decdaa08
staniel359 <staniel359@gmail.com>
2023-01-13 02:38:21 +0300

    Fix LastFM web services production bug

--- app/services/lastfm/web/base.rb
+++ app/services/lastfm/web/base.rb
@@ -10,6 +10,34 @@ module LastFM
           )
       end
 
+      def response
+        make_request(
+          link
+        )
+      end
+
+      def make_request(url)
+        RestClient::Request.execute(
+          request_data(
+            url
+          )
+        )
+      rescue RestClient::MovedPermanently => e
+        follow_redirect(e)
+      rescue RestClient::Found
+        nil
+      end
+
+      def request_data(url)
+        {
+          method: :get,
+          url:,
+          headers:,
+          proxy:,
+          max_redirects: 0
+        }
+      end
+
       def params
         {}
       end
@@ -18,8 +46,30 @@ module LastFM
         { lfmanon: '0' }
       end
 
+      def proxy
+        secrets.proxy[:ru]
+      end
+
       def param_formatted(param)
         CGI.escape(super)
+      end
+
+      def follow_redirect(redirect)
+        make_request(
+          redirect_link(
+            redirect
+          )
+        )
+      end
+
+      def redirect_link(redirect)
+        "https://www.last.fm#{redirect_path(redirect)}"
+      end
+
+      def redirect_path(redirect)
+        redirect
+          .response
+          .headers[:location]
       end
     end
   end
