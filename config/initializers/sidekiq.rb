6a09bedd8ddf376f70d759075e413d9266a4fc9b
staniel359 <staniel359@gmail.com>
2023-04-01 01:09:33 +0300

    Change Sidekiq log level for production

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,9 @@
 Sidekiq.strict_args!
 
 Sidekiq.configure_server do |config|
-  config.logger.level = :warn
+  config.logger.level = Logger::WARN
+
+  Rails.logger = Sidekiq.logger
+  ActiveRecord::Base.logger = Sidekiq.logger
+  ActiveJob::Base.logger = Sidekiq.logger
 end
