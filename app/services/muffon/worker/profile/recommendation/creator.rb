a98c49aec4f7730f78743da72932a81a327e0636
staniel359 <staniel359@gmail.com>
2023-05-30 19:35:41 +0300

    Change Sidekiq queues limits

--- app/services/muffon/worker/profile/recommendation/creator.rb
+++ app/services/muffon/worker/profile/recommendation/creator.rb
@@ -20,7 +20,7 @@ module Muffon
           end
 
           def limit_queue
-            Sidekiq::Queue[queue].limit = 3
+            Sidekiq::Queue[queue].limit = 1
           end
 
           def queue
