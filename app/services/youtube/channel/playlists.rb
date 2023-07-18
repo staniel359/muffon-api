cef0ad58c005b0e4d7a19df53618cc2593fc5389
staniel359 <staniel359@gmail.com>
2023-07-18 15:47:00 +0300

    Fix YouTube channel missing videos bug

--- app/services/youtube/channel/playlists.rb
+++ app/services/youtube/channel/playlists.rb
@@ -9,19 +9,40 @@ module YouTube
       end
 
       def channel_playlists_data
-        playlists_data.slice(
-          *%i[prev_page next_page playlists]
-        )
+        {
+          prev_page:,
+          next_page:,
+          playlists:
+        }.compact
+      end
+
+      def prev_page
+        playlists_data[
+          :prev_page
+        ]
       end
 
       def playlists_data
-        YouTube::Playlists.call(
-          channel_id: @args[:channel_id],
-          profile_id: @args[:profile_id],
-          token: @args[:token],
-          limit: @args[:limit],
-          page: @args[:page]
-        )
+        @playlists_data ||=
+          YouTube::Playlists.call(
+            channel_id: @args[:channel_id],
+            profile_id: @args[:profile_id],
+            token: @args[:token],
+            limit: @args[:limit],
+            page: @args[:page]
+          )
+      end
+
+      def next_page
+        playlists_data[
+          :next_page
+        ]
+      end
+
+      def playlists
+        playlists_data[
+          :playlists
+        ]
       end
     end
   end
--- app/services/youtube/channel/videos.rb
+++ app/services/youtube/channel/videos.rb
@@ -9,19 +9,28 @@ module YouTube
       end
 
       def channel_videos_data
-        uploads_playlist_data.slice(
-          *%i[prev_page next_page videos]
-        )
+        {
+          prev_page:,
+          next_page:,
+          videos:
+        }.compact
+      end
+
+      def prev_page
+        uploads_playlist_data[
+          :prev_page
+        ]
       end
 
       def uploads_playlist_data
-        YouTube::Playlist::Videos.call(
-          playlist_id: uploads_playlist_id,
-          profile_id: @args[:profile_id],
-          token: @args[:token],
-          limit: @args[:limit],
-          page: @args[:page]
-        )[:playlist] || {}
+        @uploads_playlist_data ||=
+          YouTube::Playlist::Videos.call(
+            playlist_id: uploads_playlist_id,
+            profile_id: @args[:profile_id],
+            token: @args[:token],
+            limit: @args[:limit],
+            page: @args[:page]
+          )[:playlist] || {}
       end
 
       def uploads_playlist_id
@@ -30,6 +39,18 @@ module YouTube
           'relatedPlaylists',
           'uploads'
         )
+      end
+
+      def next_page
+        uploads_playlist_data[
+          :next_page
+        ]
+      end
+
+      def videos
+        uploads_playlist_data[
+          :videos
+        ] || []
       end
     end
   end
