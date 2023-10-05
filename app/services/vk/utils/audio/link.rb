94259c70a405ac52f8871808ba61439d0f6c7dce
staniel359 <staniel359@gmail.com>
2023-10-05 22:24:25 +0300

    Change VK audio retrieving logic

--- app/services/vk/utils/audio/link.rb
+++ app/services/vk/utils/audio/link.rb
@@ -14,36 +14,20 @@ module VK
         end
 
         def no_data?
-          ts_data.blank?
-        end
-
-        def ts_data
-          @ts_data ||=
-            VK::Utils::Audio::Decrypter.call(
-              link: @args[:link]
-            )
+          false
         end
 
         def write_audio_data_to_file
-          write_ts_data_to_ts_file
+          convert_m3u8_file_to_ts_file
 
           convert_ts_file_to_mp3_file
 
-          delete_ts_file
+          delete_ts_file if ts_file_present?
         end
 
-        def write_ts_data_to_ts_file
-          ts_file.write(
-            ts_data
-          )
-
-          ts_file.close
-        end
-
-        def ts_file
-          @ts_file ||= ::File.open(
-            ts_file_name, 'wb'
-          )
+        def convert_m3u8_file_to_ts_file
+          `streamlink --output #{ts_file_name} -f \
+            #{@args[:link]} best`
         end
 
         def ts_file_name
@@ -57,14 +41,20 @@ module VK
         def convert_ts_file_to_mp3_file
           return if test?
 
-          VK::Utils::Audio::Converter.call(
-            file_path:
+          `ffmpeg -loglevel panic -y \
+            -i #{file_path}.ts \
+            -c copy #{file_path}.mp3`
+        end
+
+        def ts_file_present?
+          ::File.exist?(
+            ts_file_name
           )
         end
 
         def delete_ts_file
-          VK::Utils::Audio::Deleter.call(
-            file_name: ts_file_name
+          ::File.delete(
+            ts_file_name
           )
         end
       end
