357c566cce2b367579d701fe3a6fd2dc216acfed
staniel359 <staniel359@gmail.com>
2023-10-17 19:58:47 +0300

    Add Last.FM top artists artist image update logic

--- app/services/lastfm/top/artists/artist.rb
+++ app/services/lastfm/top/artists/artist.rb
@@ -13,6 +13,8 @@ module LastFM
         def data
           return artist_minimal_data if @args[:minimal]
 
+          update_artist_image
+
           self_data
             .merge(artist_data)
         end
@@ -25,13 +27,16 @@ module LastFM
           @args[:artist]
         end
 
-        def artist_data
-          {
-            source: source_data,
-            name:,
-            image: image_data,
-            listeners_count:
-          }.compact
+        def update_artist_image
+          return if test?
+
+          find_artist.update(
+            image_url:
+          )
+        end
+
+        def image_url
+          image || 'MISSING'
         end
 
         def image
@@ -40,6 +45,15 @@ module LastFM
 
         def raw_image
           artist['image']
+        end
+
+        def artist_data
+          {
+            source: source_data,
+            name:,
+            image: image_data,
+            listeners_count:
+          }.compact
         end
       end
     end
