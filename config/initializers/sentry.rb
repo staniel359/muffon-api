4637df16192045c0a214b1905b2f8382f975871e
staniel359 <staniel359@gmail.com>
2024-06-28 12:26:00 +0300

    Refactor Sentry logic

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,10 @@
-if Rails.env.production?
-  Sentry.init do |config|
-    config.dsn =
-      'https://496bf93eb9d848348ee26c94fc717336'\
-      '@o563575.ingest.sentry.io/5755700'
-  end
+return unless Rails.env.production?
+
+Sentry.init do |config|
+  config.dsn =
+    Rails
+      .application
+      .credentials
+      .sentry
+      .url
 end
