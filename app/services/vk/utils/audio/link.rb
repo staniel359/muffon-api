e4e27f3f2deb4a795c8248b2dd9f94bf730e5e7f
staniel359 <staniel359@gmail.com>
2025-02-05 22:27:20 +0300

    Change VK audio retrieving logic

--- app/services/vk/utils/audio/link.rb
+++ app/services/vk/utils/audio/link.rb
@@ -22,63 +22,67 @@ module VK
         def write_audio_data_to_file
           return if test?
 
-          # `ffmpeg \
-          #   -http_persistent false \
-          #   -i #{link} \
-          #   -y \
-          #   -movflags +faststart \
-          #   -c copy \
-          #   -loglevel error \
-          #   public/#{audio_path}`
+          `ffmpeg \
+            -http_persistent false \
+            -i #{link} \
+            -y \
+            -movflags +faststart \
+            -c copy \
+            -loglevel error \
+            public/#{audio_path}`
 
-          convert_m3u8_file_to_ts_file
+          # convert_m3u8_file_to_ts_file
 
-          convert_ts_file_to_audio_file
+          # convert_ts_file_to_audio_file
 
-          delete_ts_file if ts_file_present?
+          # delete_ts_file if ts_file_present?
         end
 
-        def convert_m3u8_file_to_ts_file
-          `lib/vk/downloader \
-            -l #{m3u8_link_formatted} \
-            -o #{ts_file_name}`
+        def link
+          @args[:link]
         end
 
-        def m3u8_link_formatted
-          @args[:link].split(
-            'index.m3u8'
-          )[0]
-        end
+        # def convert_m3u8_file_to_ts_file
+        #   `lib/vk/downloader \
+        #     -l #{m3u8_link_formatted} \
+        #     -o #{ts_file_name}`
+        # end
 
-        def ts_file_name
-          "#{file_path}.ts"
-        end
+        # def m3u8_link_formatted
+        #   @args[:link].split(
+        #     'index.m3u8'
+        #   )[0]
+        # end
 
-        def file_path
-          "public/#{audio_folder}/#{@args[:track_id]}"
-        end
+        # def ts_file_name
+        #   "#{file_path}.ts"
+        # end
 
-        def convert_ts_file_to_audio_file
-          `ffmpeg -loglevel panic -y \
-            -i #{file_path}.ts \
-            -c copy #{audio_file_name}`
-        end
+        # def file_path
+        #   "public/#{audio_folder}/#{@args[:track_id]}"
+        # end
 
-        def audio_file_name
-          "#{file_path}.#{file_extension}"
-        end
+        # def convert_ts_file_to_audio_file
+        #   `ffmpeg -loglevel panic -y \
+        #     -i #{file_path}.ts \
+        #     -c copy #{audio_file_name}`
+        # end
 
-        def ts_file_present?
-          ::File.exist?(
-            ts_file_name
-          )
-        end
+        # def audio_file_name
+        #   "#{file_path}.#{file_extension}"
+        # end
 
-        def delete_ts_file
-          ::File.delete(
-            ts_file_name
-          )
-        end
+        # def ts_file_present?
+        #   ::File.exist?(
+        #     ts_file_name
+        #   )
+        # end
+
+        # def delete_ts_file
+        #   ::File.delete(
+        #     ts_file_name
+        #   )
+        # end
       end
     end
   end
