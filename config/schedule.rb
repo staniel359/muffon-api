097eda1a6a9157a48fd2d8cf69cedb9cf130f013
staniel359 <staniel359@gmail.com>
2025-04-22 00:12:47 +0300

    infrastructure: add cron task for database backup

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,3 +5,10 @@ end
 every 1.day do
   runner 'Utils::Database::VacuumAnalyzeWorker.perform_async'
 end
+
+every 5.days do
+  command 'sudo -u muffon_api pg_dump -Fc muffon_api > ~/backup/database.dump'
+
+  # To restore
+  # sudo -u muffon_api pg_restore -C -d muffon_api ~/backup/database.dump
+end
