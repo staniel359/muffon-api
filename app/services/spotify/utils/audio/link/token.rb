9291cc2b98a2544df662a1bb7e98cfb2f7462ab5
staniel359 <staniel359@gmail.com>
2025-07-31 10:02:11 +0300

    change: spotify's audio token retrieval logic

--- app/services/spotify/utils/audio/link/token.rb
+++ app/services/spotify/utils/audio/link/token.rb
@@ -4,7 +4,7 @@ module Spotify
       class Link
         class Token < Spotify::Base
           BASE_LINK =
-            'https://open.spotify.com/api/token'.freeze
+            'http://localhost:3745/access_token'.freeze
 
           def call
             data
@@ -13,64 +13,11 @@ module Spotify
           private
 
           def data
-            response_data['accessToken']
-          end
-
-          def response
-            format_get_request(
-              link:,
-              params:,
-              headers:
-            )
+            response_data['token']
           end
 
           def link
             BASE_LINK
-          end
-
-          def params
-            {
-              reason: 'init',
-              productType: 'web-player',
-              totp: password,
-              totpServer: password,
-              totpVer: 10
-            }
-          end
-
-          def password
-            @password ||=
-              if test?
-                '842947'
-              else
-                password_data.now
-              end
-          end
-
-          def password_data
-            ROTP::TOTP.new(
-              password_secret
-            )
-          end
-
-          def password_secret
-            secrets.spotify[:totp_secret]
-          end
-
-          def headers
-            {
-              'User-Agent' => USER_AGENT,
-              'Cookie' => "sp_dc=#{sp_dc_cookie}"
-            }
-          end
-
-          def sp_dc_cookie
-            secrets
-              .spotify
-              .dig(
-                :cookies,
-                :sp_dc
-              )
           end
         end
       end
