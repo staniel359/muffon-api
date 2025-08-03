fe451e0c1be9971173159f26c4ae9a68647b2333
staniel359 <staniel359@gmail.com>
2025-08-03 10:46:07 +0300

    fix (dev): add spotify's test audio token

--- app/services/spotify/utils/audio/link/token.rb
+++ app/services/spotify/utils/audio/link/token.rb
@@ -13,7 +13,15 @@ module Spotify
           private
 
           def data
-            response_data['token']
+            if test?
+              test_token
+            else
+              response_data['token']
+            end
+          end
+
+          def test_token
+            'BQDWcGQwx7dZ_zGOoUo9hFfaLDKA4ifWng3WXMtjY3_E7prsRbUL_2aRp1RWZoM21OYh2vD_WTJQeYNbj2PvlvZDfxbsdpb6PBmFYn_2h4GNmz1Mu4FCooXAOLxQYbeo6EuSJIynbu311JYxCcsRTO-bDCt-9zE8MdzYj2yseaRxz-sOqKmvHzoBNK0mo94VZLWqJ38sCjCTIz7s1gQLl5JqieeT70C6kXcFUcJTZ8inPRnRweVSNjSd2X63MIDgKnZHXX4CsVfxQ5hyRWjwjOODlB1d8b3CXiQFzlowV2Zyj_fNY7ShL_9sXBT3gVP82mftu192VLjXeJ0pRWfLDAXwGW0UZMAuCrfxHUvdxpxN7F-uHw6kJ60z6_mrpSPDbw'
           end
 
           def link
