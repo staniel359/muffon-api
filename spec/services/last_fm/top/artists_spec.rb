9096f250ba4644920122004e8f129a9a951223f8
staniel359 <dark.pizza.42@gmail.com>
2025-09-06 17:11:56 +0300

    test: add tests for lastfm's country top logic

--- spec/services/last_fm/top/artists_spec.rb
+++ spec/services/last_fm/top/artists_spec.rb
@@ -11,5 +11,35 @@ RSpec.describe LastFM::Top::Artists do
 
       it { expect(output).to eq(Helpers::LastFM::Top.artists_data) }
     end
+
+    context 'when country present' do
+      let(:output) do
+        VCR.use_cassette(
+          'services/lastfm/top/artists/success_country'
+        ) do
+          subject.call(
+            country: 'US'
+          )
+        end
+      end
+
+      it { expect(output).to eq(Helpers::LastFM::Top.artists_country_data) }
+    end
+  end
+
+  describe 'no processing' do
+    context 'when wrong country' do
+      let(:output) do
+        VCR.use_cassette(
+          'services/lastfm/top/artists/wrong_country'
+        ) do
+          subject.call(
+            country: random_string
+          )
+        end
+      end
+
+      it { expect { output }.to raise_error(not_found_error) }
+    end
   end
 end
--- spec/services/last_fm/top/tracks_spec.rb
+++ spec/services/last_fm/top/tracks_spec.rb
@@ -11,5 +11,35 @@ RSpec.describe LastFM::Top::Tracks do
 
       it { expect(output).to eq(Helpers::LastFM::Top.tracks_data) }
     end
+
+    context 'when country present' do
+      let(:output) do
+        VCR.use_cassette(
+          'services/lastfm/top/tracks/success_country'
+        ) do
+          subject.call(
+            country: 'US'
+          )
+        end
+      end
+
+      it { expect(output).to eq(Helpers::LastFM::Top.tracks_country_data) }
+    end
+  end
+
+  describe 'no processing' do
+    context 'when wrong country' do
+      let(:output) do
+        VCR.use_cassette(
+          'services/lastfm/top/tracks/wrong_country'
+        ) do
+          subject.call(
+            country: random_string
+          )
+        end
+      end
+
+      it { expect { output }.to raise_error(not_found_error) }
+    end
   end
 end
