0dfcb29d8579716f3e44dbc1b3b4b8ab2594bd5b
staniel359 <dark.pizza.42@gmail.com>
2026-03-14 14:23:44 +0300

    change: add extra data for youtubemusic requests

--- app/services/youtubemusic/base.rb
+++ app/services/youtubemusic/base.rb
@@ -12,21 +12,24 @@ module YouTubeMusic
       {
         **super,
         'X-Goog-Visitor-Id' =>
-          'CgtjcXJrbFFtY24wYyjy8LTNBjIKCgJCWRIEGgAgHQ%3D%3D',
+          'CgtoTGtFbEc5UEYtZyihh9XNBjIKCgJHQhIEGgAgaGLfAgrcAjE2LllUPU5QUFp4dU91N28tQWQxMk5Vdl9rTl9fLW5WVEZBS3BnSS0xb1p6YUpZSlpGaGlidnprY3RvUVptZUtZeFFGUE84a2pVX0dLUjAwaVZXemJab19tZlppeS11U2lpQUpzY3IyT1plSkNqOGpzbjcwOW94NDY3VlFzZ281VnFLcS1YOS12aERNR2syWF80cTlpREU5T3VEbWtqNlllemJ0X2paaktxbFgzRW1QTF9SekN3aFFuc2Y1OFZYVU5HQi1FMHYzeVZsdW9Wb1BiVTRTTUxWd1JLZXhRcENydnlUVlExaUl3WUZuOWJmVDljQ0R3cE1zdEFFdGliRUdFWTJKNFpMVVlqV2dyT1VrMWRRRS1oZ1RLdC1QMkM2VW5RZWtvY0kwSWZNMUNIMEZoSkRWbk10UmhibEowN3JJQnJQMWl5M1BIUGpkUmxmUTBOWHVFb2VUaW4ydw%3D%3D',
         'X-Youtube-Bootstrap-Logged-In' => 'false',
         'X-Youtube-Client-Name' => '67',
-        'X-Youtube-Client-Version' => '1.20260304.03.00'
+        'X-Youtube-Client-Version' => '1.20260311.03.00',
+        'Referer' => 'https://music.youtube.com'
       }
     end
 
     # rubocop:disable Layout/LineLength
     def cookies
       {
-        '__Secure-ROLLOUT_TOKEN' => 'CL-u9pzcruLp4QEQoYfAwqyOkwMYqeiRxayOkwM=',
-        '__Secure-YNID' => '16.YT=i2mygmBu25vuVDSBz3rcpbUX58T8EDmhC6PLd6nbGeLmn2QYtcmHAR0GMmOjOM0YpxaZGVdr07HqwsY577eXjJpYG3DzvQx5_diHrUTIGNM18wKzNNGYp3xFCiuIh1a_5XPnP6dyjj98BHrRdaXVuCIqRJJ_gRS84vvxvkiwcmWfFdPXvy1BJdtDcih37bfIJWi62bABneKgpOD-EPqXWsn0q3d4yRkey7OPrqQBwLt0-ncpEHRuxRRLqY7vzHlNINeEgxEPYYlZlkoJoBSbcOm6SIoHfZFbR1GgR5npYoPbzkv8mWDKYJejPp4caHAXf4ySOIMa_aigr4TxOjAgIQ',
-        'VISITOR_INFO1_LIVE' => 'x5bEMLtEr4o',
-        'VISITOR_PRIVACY_METADATA' => 'CgJCWRIEGgAgDA==',
-        'YSC' => 'h1zewZWCYfo'
+        '__Secure-ROLLOUT_TOKEN' => 'CNGmmZiBwa2IkwEQsPS9-aGfkwMYhYDS_aGfkwM=',
+        '__Secure-YNID' => '16.YT=W3P7dRt2_-T1FRjJFkYP3lXqi77dPYPo0KNpuacliJWxY3Iv-fN9wLYXHkNsT3Wx_vOA5h_GsL-nczN5RfFUages7VV8l36oXmHAETJPob5vtuoLKhmavneoE8c195kDozCOihc6zYNEklhyMi9RQilc9FC0Y3OGfSqL7VRNpZY3EBHb1poruRSHNIdCT6WwdiN0QmyX59Rstu5M-KUK2AOMDNdpHtnhUVbb4CUGJIavGwbT9WrmXmt4jcPDnfSMwryiFun5yNM8eYeqs1icrBZhw16UmKTYEJd_hO7lKbbVnh22Wu70bm5ItSeVYmpEUQyQw4onWovDwFaNn8HNnQ',
+        '_gcl_au' => '1.1.1024733403.1773486950',
+        'SOCS' => 'CAISNQgREitib3FfaWRlbnRpdHlmcm9udGVuZHVpc2VydmVyXzIwMjYwMzExLjA2X3AwGgJlbiACGgYIgMrSzQY',
+        'VISITOR_INFO1_LIVE' => 'hLkElG9PF-g',
+        'VISITOR_PRIVACY_METADATA' => 'CgJHQhIEGgAgaA==',
+        'YSC' => '_zMhf_yedNc'
       }
     end
     # rubocop:enable Layout/LineLength
