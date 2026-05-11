860902a85be891300e2499ec3022d7b4bcc3c229
staniel359 <dark.pizza.42@gmail.com>
2026-05-12 00:27:55 +0300

    chore: remove bulk updates in migrations

--- db/migrate/20260414155138_change_listeners_count_columns_default_for_artists_and_albums.rb
+++ db/migrate/20260414155138_change_listeners_count_columns_default_for_artists_and_albums.rb
@@ -1,18 +1,10 @@
 class ChangeListenersCountColumnsDefaultForArtistsAndAlbums < ActiveRecord::Migration[8.1]
   def change
-    Artist
-      .where(listeners_count: nil)
-      .update_all(listeners_count: 0)
-
     change_column :artists,
       :listeners_count,
       :integer,
       null: false,
       default: 0
-
-    Album
-      .where(listeners_count: nil)
-      .update_all(listeners_count: 0)
 
     change_column :albums,
       :listeners_count,
