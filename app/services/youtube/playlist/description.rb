00f8700e3434c936933564d092e077bfdacb8b38
staniel359 <dark.pizza.42@gmail.com>
2026-05-29 01:10:06 +0300

    fix (youtube): update video playlist description in video playlist description service

--- app/services/youtube/playlist/description.rb
+++ app/services/youtube/playlist/description.rb
@@ -4,6 +4,8 @@ module YouTube
       private
 
       def playlist_data
+        update_video_playlist_record!
+
         Muffon::Formatter::Source::VideoPlaylist::Description.call(
           video_playlist_record:
         )
